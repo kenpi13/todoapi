3d4e343485e5a0f81c3056b61afc48eb7feaf65b
kenpi13 <kenkenpi0526@gmail.com>
2020-10-17 16:03:37 +0900

    initial commit

---
+++ src/main/kotlin/com/example/todoapi/TodoapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.todoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoapiApplication>(*args)
+}
